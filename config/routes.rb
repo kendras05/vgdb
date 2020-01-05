c73b54c9d5574ffef2831f03b27d0135f22a320c
user <user01@KMS-MacBook.local>
2020-01-04 21:38:29 -0800

    page to show all 100 games

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homepg#index'
+  resources :homepg
+  resources :secpg
+  get '/games/:id', to: 'secpg#index', as: 'game'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
